d5db0c2274f0ab6ebc2d08b59bcdb006fed7f893
student <student@K09-10>
2018-06-26 17:02:40 +0300

    1.3

---
+++ 01-Goods-Catalog/AddWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace _01_Goods_Catalog
+{
+    /// <summary>
+    /// Interaction logic for AddWindow.xaml
+    /// </summary>
+    public partial class AddWindow : Window
+    {
+        public AddWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void addButton_Click(object sender, RoutedEventArgs e)
+        {
+            string name = category.Text;
+            if (String.IsNullOrEmpty(name))
+                MessageBox.Show("Вы не ввели название категории", "Предупреждение", MessageBoxButton.OK, MessageBoxImage.Warning);
+            else
+            {
+                string path = @"..\..\Data\Categories.xml";
+                XDocument doc = XDocument.Load(path);
+                var root = doc.Element("root");
+                var categories = root.Elements("category");
+                int k = categories.Count();
+                XElement newElem = new XElement("category", 
+                    new XAttribute("id", k),
+                    new XAttribute("name", name));
+                root.Add(newElem);
+                doc.Save(path);
+                MessageBox.Show("Категория успешно добавлена", "Сообщение", MessageBoxButton.OK, MessageBoxImage.Information);
+                this.DialogResult = true;
+            }
+        }
+    }
+}
--- 01-Goods-Catalog/MainWindow.xaml.cs
+++ 01-Goods-Catalog/MainWindow.xaml.cs
@@ -27,6 +27,41 @@ namespace _01_Goods_Catalog
 
         private void filter_Click(object sender, RoutedEventArgs e)
         {
+            int k1 = listCategory.SelectedIndex;
+            int k2 = listProducer.SelectedIndex;
+
+            XmlDataProvider xdp = (XmlDataProvider)FindResource("productProvider");
+            Binding b = new Binding();
+
+            b.Source = xdp;
+            if (k1 > 0 && k2 == 0)
+                b.XPath = $"product[@cid={k1}]";
+            else if (k1 == 0 && k2 > 0)
+                b.XPath = $"product[@pid={k2}]";
+            else if(k1 > 0 && k2 > 0)
+                b.XPath = $"product[@cid={k1} and @pid={k2}]";
+            else
+                b.XPath = $"product";
+            productList.SetBinding(ListView.ItemsSourceProperty, b);
+
+        }
+
+        private void order_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void addcategory_Click(object sender, RoutedEventArgs e)
+        {
+            AddWindow win = new AddWindow();
+            if(win.ShowDialog() == true)
+            {
+                XmlDataProvider xdp = (XmlDataProvider)FindResource("categoryProvider");
+                Binding b = new Binding();
+                b.Source = xdp;
+                b.XPath = "category";
+                listCategory.SetBinding(ComboBox.ItemsSourceProperty, b);
+            }
             
         }
     }
